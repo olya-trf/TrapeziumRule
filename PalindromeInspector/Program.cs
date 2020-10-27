d3ea3b6998d85bd2550abce57cb59a9331a7f406
olya-trf <71277854+olya-trf@users.noreply.github.com>
2020-10-27 08:49:38 +0300

    Add files via upload

---
+++ PalindromeInspector/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Определить метод, определяющий является ли строка (слово)
+палиндромом */
+
+using System;
+
+namespace PalindromeInspector
+{
+    class Program
+    {
+        static string PalindromeReverse(string str)
+        {
+            string rts = "";
+            for (int i = str.Length - 1; i >= 0; --i)
+            {
+                rts = rts + str[i];
+            }
+            return rts;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово для проверки > ");
+            var str = Console.ReadLine();
+            var rts = PalindromeReverse(str);
+            if (str.ToLower() != rts.ToLower()) Console.WriteLine(str + " -- не палиндром.");
+            else Console.WriteLine(str + " -- палиндром.");
+        }
+    }
+}
